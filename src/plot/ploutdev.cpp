4f58885211af9228986ff9b4724a78172a9d5f35
Aron Dobos <aron.dobos@outlook.com>
2016-03-04 21:29:22 +0000

    remove test of rectangle rendering fix - didn't work well.
    git-svn-id: https://efmsvn2.nrel.gov/wex/svn/trunk@481 e21c72fa-1056-4880-a922-3284d9a13290

--- src/plot/ploutdev.cpp
+++ src/plot/ploutdev.cpp
@@ -299,11 +299,7 @@ void wxPLDCOutputDevice::Polygon( size_t n, const wxRealPoint *pts, Style sty ) 
 	m_curdc->DrawPolygon( n, &ipt[0], 0, 0, sty==ODDEVEN ?  wxODDEVEN_RULE : wxWINDING_RULE );
 }
 void wxPLDCOutputDevice::Rect( double x, double y, double width, double height ) {
-	int pw = CAST(width);
-	int ph = CAST(height);
-	if ( pw < 1 ) pw = 1;
-	if ( ph < 1 ) ph = 1;
-	m_curdc->DrawRectangle( CAST(x), CAST(y), pw, ph );
+	m_curdc->DrawRectangle( CAST(x), CAST(y), CAST(width), CAST(height) );
 }
 void wxPLDCOutputDevice::Circle( double x, double y, double radius ) {
 	int irad = CAST(radius);
