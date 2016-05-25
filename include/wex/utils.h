5a56503acedd5d2b45a2c3525c45ff1c2771535e
Aron Dobos <aron.dobos@outlook.com>
2016-05-25 14:50:47 +0000

    moved md5 checksum code back to wex but put in utils to avoid conflicting with md5 object from freetype
    git-svn-id: https://efmsvn2.nrel.gov/wex/svn/trunk@555 e21c72fa-1056-4880-a922-3284d9a13290

--- include/wex/utils.h
+++ include/wex/utils.h
@@ -98,5 +98,37 @@ void wxSortByLabels(wxArrayString &names, wxArrayString &labels);
 // create a transparent overlay frame
 wxFrame *wxCreateTransparentOverlay( wxWindow *parent );
 
+
+/*
+ **********************************************************************
+ ** Copyright (C) 1990, RSA Data Security, Inc. All rights reserved. **
+ **                                                                  **
+ ** License to copy and use this software is granted provided that   **
+ ** it is identified as the "RSA Data Security, Inc. MD5 Message     **
+ ** Digest Algorithm" in all material mentioning or referencing this **
+ ** software or this function.                                       **
+ **                                                                  **
+ ** License is also granted to make and use derivative works         **
+ ** provided that such works are identified as "derived from the RSA **
+ ** Data Security, Inc. MD5 Message Digest Algorithm" in all         **
+ ** material mentioning or referencing the derived work.             **
+ **                                                                  **
+ ** RSA Data Security, Inc. makes no representations concerning      **
+ ** either the merchantability of this software or the suitability   **
+ ** of this software for any particular purpose.  It is provided "as **
+ ** is" without express or implied warranty of any kind.             **
+ **                                                                  **
+ ** These notices must be retained in any copies of any part of this **
+ ** documentation and/or software.                                   **
+ **********************************************************************
+ */
+//! Utilities to calculate MD5 checksums from files or strings.
+//! Returns the MD5 checksum for the given file
+wxString wxGetFileMD5(wxInputStream &str);
+wxString wxGetFileMD5(const wxString &filename);
+//! Returns the MD5 for the given string.
+wxString wxGetMD5(const wxString &str);
+
+
 #endif
 
