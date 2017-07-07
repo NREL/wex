53c55e52f9237b58a4f53f47e3587eaccee0332a
Evan Weaver <evan.weaver@nrel.gov>
2017-07-07 12:35:28 -0600

    Fix wonky merge.

--- include/wex/dview/dvstatisticstablectrl.h
+++ include/wex/dview/dvstatisticstablectrl.h
@@ -1,56 +1,30 @@
 /***********************************************************************************************************************
-
 *  WEX, Copyright (c) 2008-2017, Alliance for Sustainable Energy, LLC. All rights reserved.
-
 *
-
 *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
-
 *  following conditions are met:
- 
 *
-
 *  (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following
-
 *  disclaimer.
-
 *
-
 *  (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
-
 *  following disclaimer in the documentation and/or other materials provided with the distribution.
-
 *
-
 *  (3) Neither the name of the copyright holder nor the names of any contributors may be used to endorse or promote
-
 *  products derived from this software without specific prior written permission from the respective party.
-
 *
-
 *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-
 *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-
 *  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER, THE UNITED STATES GOVERNMENT, OR ANY CONTRIBUTORS BE LIABLE FOR
-
 *  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-
 *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
-
 *  AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-
 *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 **********************************************************************************************************************/
-
-
 
 #ifndef __DVStatisticsTableCtrl_h
 
 #define __DVStatisticsTableCtrl_h
-
-
 
 /*
 
@@ -62,21 +36,15 @@
 
 */
 
-
-
 // For compilers that support precompilation, includes "wx/wx.h".
 
 #include <wx/wxprec.h>
-
-
 
 #ifndef WX_PRECOMP
 
 #include <wx/wx.h>
 
 #endif
-
-
 
 #include <wx/dataview.h>
 
@@ -86,39 +54,26 @@
 
 #include <wx/stream.h>
 
-
-
 #include "wex/numeric.h"
 
 #include "wex/dview/dvtimeseriesdataset.h"
 
-
-
 enum { ID_STATISTICS_CTRL = 50, };
-
-
 
 class wxDVVariableStatistics
 
 {
-
 public:
 
 	wxDVVariableStatistics(wxDVStatisticsDataSet *ds, wxString GroupName, bool OwnsDataset = false);
 
-
-
 	~wxDVVariableStatistics();
-
-
 
 	StatisticsPoint At(size_t i, double m_offset, double m_timestep) const;
 
 	wxDVStatisticsDataSet *GetDataSet() const { return m_data; }
 
 	wxString GetGroupName();
-
-
 
 private:
 
@@ -127,15 +82,11 @@ private:
 	bool m_ownsDataset;
 
 	wxString m_groupName;
-
 };
-
-
 
 class dvStatisticsTreeModelNode
 
 {
-
 public:
 
 	dvStatisticsTreeModelNode(dvStatisticsTreeModelNode* parent, wxString variableName);
@@ -146,11 +97,7 @@ public:
 
 	~dvStatisticsTreeModelNode();
 
-
-
 	bool IsContainer() const;
-
-
 
 	dvStatisticsTreeModelNode* GetParent();
 
@@ -163,8 +110,6 @@ public:
 	unsigned int GetChildCount() const;
 
 	void RemoveAllChildren();
-
-
 
 	wxString GetName();
 
@@ -181,8 +126,6 @@ public:
 	double GetAvgDailyMin();
 
 	double GetAvgDailyMax();
-
-
 
 private:
 
@@ -207,34 +150,24 @@ private:
 	dvStatisticsTreeModelNode *m_parent;
 
 	std::vector<dvStatisticsTreeModelNode*> m_children;
-
 };
-
-
 
 class dvStatisticsTreeModel : public wxDataViewModel
 
 {
-
 public:
 
 	dvStatisticsTreeModel();
 
 	~dvStatisticsTreeModel() { delete m_root; }
 
-
-
 	void Refresh(std::vector<wxDVVariableStatistics*> stats, bool showMonths);
 
 	wxDataViewItem GetRoot();
 
-
-
 	// override sorting to always sort branches ascendingly
 
 	int Compare(const wxDataViewItem &item1, const wxDataViewItem &item2, unsigned int column, bool ascending) const;
-
-
 
 	// implementation of base class virtuals to define model
 
@@ -246,7 +179,7 @@ public:
 
 	virtual bool SetValue(const wxVariant &variant, const wxDataViewItem &item, unsigned int col);
 
-	virtual bool IsEnabled(const wxDataViewItem &, unsigned int ) const { return false; }
+	virtual bool IsEnabled(const wxDataViewItem &, unsigned int) const { return false; }
 
 	virtual wxDataViewItem GetParent(const wxDataViewItem &item) const;
 
@@ -254,27 +187,19 @@ public:
 
 	virtual unsigned int GetChildren(const wxDataViewItem &parent, wxDataViewItemArray &array) const;
 
-
-
 private:
 
 	dvStatisticsTreeModelNode*   m_root;
-
 };
-
-
 
 class wxDVStatisticsTableCtrl : public wxPanel
 
 {
-
 public:
 
 	wxDVStatisticsTableCtrl(wxWindow *parent, wxWindowID id);
 
 	virtual ~wxDVStatisticsTableCtrl();
-
-
 
 	void RebuildDataViewCtrl();
 
@@ -288,13 +213,9 @@ public:
 
 	void Invalidate();
 
-
-
 	wxMenu &GetContextMenu() { return m_contextMenu; }
 
 	void OnShowMonthsClick(wxCommandEvent &e);
-
-
 
 private:
 
@@ -306,15 +227,11 @@ private:
 
 	wxDataViewColumn* m_col;
 
-
-
 	wxMenu m_contextMenu;
 
 	wxCheckBox *m_chkShowMonths;
 
 	bool m_showMonths;
-
-
 
 	// event handlers
 
@@ -326,12 +243,7 @@ private:
 
 	void OnContextMenu(wxDataViewEvent& event);
 
-
-
 	DECLARE_EVENT_TABLE();
-
 };
-
-
 
 #endif
