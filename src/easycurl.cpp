73b0ee85746fd3f26f499c6f90680d95f24f7b30
Paul Gilman <cpaulgilman@gmail.com>
2018-10-19 11:55:56 -0700

    remove keys

--- src/easycurl.cpp
+++ src/easycurl.cpp
@@ -587,18 +587,6 @@ void wxEasyCurl::SetApiKeys(const wxString &google, const wxString &bing)
 	if (!bing.IsEmpty()) BING_API_KEY = bing;
 }
 
-// Google APIs:
-// login to developer api console at: https://code.google.com/apis/console
-// user name: aron.dobos@nrel.gov
-// passwd: 1H*****....******r
-//static wxString GOOGLE_API_KEY("AIzaSyCyH4nHkZ7FhBK5xYg4db3K7WN-vhpDxas");
-
-// Bing Map APIs:
-// login to account center at: https://www.bingmapsportal.com/
-// user name: aron.dobos@nrel.gov
-// passwd: 1H*****....******r
-//static wxString BING_API_KEY("Av0Op8DvYGR2w07w_771JLum7-fdry0kBtu3ZA4uu_9jBJOUZgPY7mdbWhVjiORY");
-
 bool wxEasyCurl::GeoCode(const wxString &address, double *lat, double *lon, double *tz)
 {
 	wxBusyCursor _curs;
