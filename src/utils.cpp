c344456f26c50d56cfefa4293fbee1179635fcfd
Steven Janzou <sjanzou@gmail.com>
2023-02-02 03:54:23 -0700

    Multiline strings working with updated helper functions in utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -84,28 +84,28 @@ wxString Read_JSON_value(const rapidjson::Value& doc, wxString name)
     return wxString::FromUTF8(doc[(const char*)name.mb_str()].GetString());
 }
 
-void Write_JSON_multiline_value(rapidjson::Document& doc, wxString name, wxString value) // unicode fails in arraystring writing "\\" does not goe to "\\\\"
+void Write_JSON_multiline_value(rapidjson::Document& doc, wxString name, wxString value) // json array fails in arraystring writing "\\" does not go to "\\\\"
 {
-    rapidjson::Value json_val;
-    // break into array value for json
-    json_val.SetArray();
+    rapidjson::Document json_multiline(&doc.GetAllocator()); // for table inside of json document.
+    json_multiline.SetObject();
     wxArrayString as = wxSplit(value, '\n');
     for (size_t i = 0; i < as.Count(); i++) {
-//        rapidjson::Value jv;
-//        jv.SetString(as[i].utf8_str(), doc.GetAllocator());
-//        json_val.PushBack(jv, doc.GetAllocator());
-        json_val.PushBack(rapidjson::Value(as[i].utf8_str(), doc.GetAllocator()).Move(), doc.GetAllocator());
+        wxString linename = wxString::Format("%d",(int)i+1);
+        as[i].Replace("'\\'", "'\\\\'"); // 		file = replace(file, '\\', '/');
+        as[i].Replace("^\\", "^\\\\");  // 	plotopt({"title"=sprintf('IV curves at %g ^\\deg  C',Tc),"popup"=true,"backcolor"=[255,255,255],"legend"=true, 'legendpos'='bottom'});
+        Write_JSON_value(json_multiline, linename, as[i]);
     }
-    doc.AddMember(rapidjson::Value(name.c_str(), (unsigned int)name.size(), doc.GetAllocator()).Move(), json_val.Move(), doc.GetAllocator());
+    doc.AddMember(rapidjson::Value(name.c_str(), (unsigned int)name.size(), doc.GetAllocator()).Move(), json_multiline.Move(), doc.GetAllocator());
 }
 
 wxString Read_JSON_multiline_value(const rapidjson::Value& doc, wxString name)
 {
     wxArrayString as;
-    auto& json_val = doc[(const char*)name.mb_str()];
-    for (rapidjson::SizeType i = 0; i < json_val.Size(); i++) {
-//        as.push_back(wxString::FromUTF8(json_val[i].GetString()));
-        as.push_back((json_val[i].GetString()));
+    auto& json_multiline = doc[(const char*)name.mb_str()];
+
+    for (rapidjson::Value::ConstMemberIterator itr = json_multiline.MemberBegin(); itr != json_multiline.MemberEnd(); ++itr) {
+        wxString linename = itr->name.GetString();
+        as.push_back(Read_JSON_value(json_multiline, linename));
     }
     wxString str =  wxJoin(as,'\n');
     return str;
